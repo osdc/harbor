333801743d1cd58b7902c5ecd820b2cc24d08d0a
saranshbarua <saranshbarua@gmail.com>
2018-08-15 03:45:12 +0530

    User interface phase 1 complete

--- harbor.js
+++ harbor.js
@@ -1,1 +1,15 @@
-alert('Harbor: A Cyberoam client');
+// alert('Harbor: A Cyberoam client');
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: ''},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  });
